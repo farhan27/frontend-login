ce8091e0b8b1b255bc03dd6e62e223077db3caa2
farhan27 <farhanjalal92@gmail.com>
2022-12-21 14:06:03 +0800

    add image drag and drop

---
+++ src/components/drag-and-drop/dragDropImage.js
@@ -0,0 +1,24 @@
+import { useDrag } from "react-dnd"
+
+export const DragAndDropImage = ({ id, src,className, width }) => {
+    const [{ isDragging }, drag] = useDrag(() => (
+        {
+            type: "image",
+            item:{
+                id:id
+            },
+            collect: (monitor) => ({
+                isDragging: !!monitor.isDragging()
+            })
+        }
+    ))
+    return <div>
+        <img
+            ref={drag}
+            src={src}
+            id={id} 
+            width={width ? width : ''}
+            className={className ? className : ''}
+            style={{ border: isDragging ? "5px solid" : "0px" }} />
+    </div>
+}
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,8 @@
-import {  useState } from "react"
+import { useState } from "react"
 import { useDrop } from "react-dnd"
 
 import { DragAndDrop } from "../components/drag-and-drop/dragDrop"
+import { DragAndDropImage } from "../components/drag-and-drop/dragDropImage"
 
 const labelList = [
     {
@@ -32,25 +33,48 @@ const labelList = [
 
 ]
 
+const imageAndTableList = [
+    {
+        id: 1,
+        src: "/table.PNG",
+        column: 'column1'
+    },
+]
+
 export const Dashboard = () => {
     const [labelListData, setLabelListData] = useState(labelList)
+    const [imageAndTableListData, setimageAndTableListData ] = useState(imageAndTableList)
 
     const [{ isOver }, drop] = useDrop(() => ({
         accept: "div",
+        drop: (item) => addLabelToBoard(item.id),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+        })
+    }))
+
+    const [{ isOverImage }, dropImage] = useDrop(() => ({
+        accept: "image",
         drop: (item) => addImageToBoard(item.id),
         collect: (monitor) => ({
             isOver: !!monitor.isOver()
         })
     }))
 
-    const addImageToBoard = (id) => {
+
+    const addLabelToBoard = (id) => {
         console.log(id);
         const label = labelListData.filter((label) => label.id === id);
         label[0].status = 'drop';
         setLabelListData(labelListData.filter((label) => label.id !== id).concat(label[0]))
-        // const newLabelList = labelListData.filter((label) => id === label.id);
-        // console.log('newLabelList', newLabelList)
-        // setBoard((board) => [...board, newLabelList[0]])
+
+    }
+
+    const addImageToBoard = (id) => {
+        console.log(id);
+        const image = imageAndTableList.filter((image) => image.id === id);
+        image[0].column = 'column2';
+        setimageAndTableListData(imageAndTableList.filter((image) => image.id !== id).concat(image[0]))
 
     }
 
@@ -59,7 +83,7 @@ export const Dashboard = () => {
 
 
             <div className="grid grid-rows-2 grid-flow-col gap-4 justify-items-center">
-                <div className="grid grid-cols-5 gap-4">
+                <div className="grid grid-cols-6 gap-4">
                     <div style={{ height: '500px' }} className="bg-slate-200 m-2 p-2 border-2 rounded col-span-1">
                         <p className="text-neutral-900">Label List</p>
                         {
@@ -68,7 +92,7 @@ export const Dashboard = () => {
                                     <DragAndDrop
                                         key={label.id.toString()}
                                         id={label.id}
-                                        className={"w-32 bg-red-500 mr-2 mb-2 p-2 rounded flex justify-center"}
+                                        className={"w-32 bg-red-500 mr-2 mb-2 p-2 rounded flex justify-center cursor-pointer"}
                                         name={label.name} />
                                 )
                             })
@@ -90,20 +114,23 @@ export const Dashboard = () => {
                             }
                         </div>
 
-                        <div style={{ width: '800px', height: '400px' }} className="m-2 p-2 border-2 rounded bg-slate-200">
-                            {/* {
-                                labelListData.filter((label) => label.status === 'draggable').map((label) => {
-                                    return (
-                                        <DragAndDrop 
-                                        key={label.id.toString()} 
-                                        id={label.id} 
-                                        className={"w-32 bg-red-500 inline-flex mr-2 p-2 rounded flex justify-center"}
-                                        name={label.name} />
-                                    )
+                        <div style={{ width: '800px', height: '420px' }} className="m-2 p-2 border-2 rounded bg-slate-200" ref={dropImage}>
+                            {
+                                imageAndTableListData.filter((label) => label.column === 'column2').map((picture) => {
+                                    return <DragAndDropImage key={picture.id.toString()} src={picture.src} id={picture.id} className={'w-full'} />
                                 })
-                            } */}
+                            }
 
                         </div>
+                    </div>
+                    <div style={{ height: '500px' }} className="bg-slate-200 m-2 p-2 border-2 rounded col-span-1">
+                        <p className="text-neutral-900">Image and Table List</p>
+                        {
+                            imageAndTableListData.filter((label) => label.column === 'column1').map((picture) => {
+                                return <DragAndDropImage key={picture.id.toString()} src={picture.src} id={picture.id} width={'150px'} />
+                            })
+                        }
+
                     </div>
                 </div>
 
